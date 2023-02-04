3cf3430137c7a968fdaac19ce0145c9a198540e4
jep <walangsigit@gmail.com>
2023-02-04 13:33:50 +0700

    feat: initial commit TP1

---
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -0,0 +1,19 @@
+package assignments.assignment1;
+
+public class NotaGenerator {
+
+    /*
+     * method main
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static String generateId(String nama, String nomorHP){
+        return null;
+    }
+
+    public static String generateNota(String id, String paket, String nama, String tanggal){
+        return null;
+    }
+}
---
+++ assignment1/src/test/java/assignments/assignment1/NotaGeneratorTest.java
@@ -0,0 +1,22 @@
+package assignments.assignment1;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class NotaGeneratorTest {
+    @Test
+    public void testGenerateNota1(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        String dateNow = dtf.format(now);
+        assertEquals("", NotaGenerator.generateNota("", "", "", dateNow));
+    }
+
+    @Test
+    public void testGenerateId1(){
+        assertEquals("", NotaGenerator.generateId("", ""));
+    }
+
+}
